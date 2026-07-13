115fdfe74f27912c857a79a7f0e17d22cac98786
berluk12@gmail.com <berluk12@gmail.com>
2026-07-13 18:34:26 +0200

    Refactor FilesPage to update stream download URL for public files

--- src/pages/files/FilesPage.tsx
+++ src/pages/files/FilesPage.tsx
@@ -144,7 +144,7 @@ export async function createStreamUrl(fileId: string): Promise<string> {
         }
     )
 
-    return `/files/stream-download?downloadItemUuid=${res.data}`
+    return `/api/files/stream-download?downloadItemUuid=${res.data}`
 }
 
 // ---- Upload Dialog ----
