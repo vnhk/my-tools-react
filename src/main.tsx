0f0847964f655ef823abbad29be4e9b19a741896
berluk12@gmail.com <berluk12@gmail.com>
2026-07-29 22:26:50 +0200

    Refactor App component to simplify structure, removing unused imports,

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,4 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import App from './App.tsx'
-import { getEntityConfigs } from './api/entityConfig'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-getEntityConfigs().catch(() => {})
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
