3bbc69e59473f72bde13b499a3bc6ac90d83d4d1
berluk12@gmail.com <berluk12@gmail.com>
2026-07-29 21:38:31 +0200

    Add build target configuration to vite.config.ts; retain server proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://127.0.0.1:8091',
-        secure: false,
-      },
-      '/storage': {
-        target: 'https://127.0.0.1:8091',
-        secure: false,
-      },
-      '/ws': {
-        target: 'https://127.0.0.1:8091',
-        secure: false,
-        ws: true,
-      },
+    plugins: [react()],
+    build: {
+
+        target: 'chrome69'
+
     },
-  },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://127.0.0.1:8091',
+                secure: false,
+            },
+            '/storage': {
+                target: 'https://127.0.0.1:8091',
+                secure: false,
+            },
+            '/ws': {
+                target: 'https://127.0.0.1:8091',
+                secure: false,
+                ws: true,
+            },
+        },
+    },
 })
